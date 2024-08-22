5e085d0734458a8ac244b074b96daded497877e3
Christian Carollo <Administrator@DESKTOP-95RB6IG>
2024-08-22 16:01:37 +0200

    feat: added RateLimiter and Bulkhead annotations to improve resiliency.
    - Implemented @RateLimiter to limit the number of requests allowed in a time period.
    - Added @Bulkhead to manage the maximum number of concurrent calls to the remote service.
    - Configured resiliency policies in application.yml to optimize the behavior of retry, circuit breaker, rate limiter and bulkhead.

--- resilience/src/main/java/com/example/resilience/CircuitBreakerController.java
+++ resilience/src/main/java/com/example/resilience/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.example.resilience;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +17,10 @@ public class CircuitBreakerController {
     private final Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/recall")
-    @Retry(name = "recall")
+    @Retry(name = "recall", fallbackMethod = "fallbackRecall")
     @CircuitBreaker(name = "recall", fallbackMethod = "fallbackRecall")
+    @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String recall(){
         logger.info("Enter to CircuitBreakerController/retry");
         ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8765/servicea", String.class);
