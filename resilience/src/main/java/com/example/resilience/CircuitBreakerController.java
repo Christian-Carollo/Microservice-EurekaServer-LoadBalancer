9fe2c70e936d1db7362afa92aabf3891aa209bd2
Christian Carollo <Administrator@DESKTOP-95RB6IG>
2024-08-22 12:38:01 +0200

    feat: implemented Circuit Breaker and Retry to handle resilience of service calls.
    - Added @CircuitBreaker to handle persistent errors in remote service calls.
    - Added fallback method to provide fallback response when service is unavailable.
    - Configured @Retry to retry the request in case of temporary failure.

---
+++ resilience/src/main/java/com/example/resilience/CircuitBreakerController.java
@@ -0,0 +1,30 @@
+package com.example.resilience;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    private final Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/recall")
+    @Retry(name = "recall")
+    @CircuitBreaker(name = "recall", fallbackMethod = "fallbackRecall")
+    public String recall(){
+        logger.info("Enter to CircuitBreakerController/retry");
+        ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8765/servicea", String.class);
+        return response.getBody();
+    }
+
+    public String fallbackRecall(Exception ex){
+        logger.info("Fallback executed due to: " + ex);
+        return "Service is currently unavailable. Please try again later";
+    }
+}
